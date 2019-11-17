798fb80c07e4a2be251380a22f734570f63531a8
DCOFFEY20 <dcoffey20@gmail.com>
2019-11-17 11:20:32 -0600

    set the time for each time block of the planner

--- script.js
+++ script.js
@@ -11,9 +11,15 @@ $(document).ready(function () {
     let test = moment().format("H");
     console.log(test);
 
-
-   
-
+//setting the correct time for each hour
+$("#timeBlock7 .time-block").text("10 AM");
+$("#timeBlock6 .time-block").text("11 AM");
+$("#timeBlock5 .time-block").text("12 PM");
+$("#timeBlock4 .time-block").text("1 PM");
+$("#timeBlock3 .time-block").text("2 PM");
+$("#timeBlock2 .time-block").text("3 PM");
+$("#timeBlock1 .time-block").text("4 PM");
+$("#timeBlock0 .time-block").text("5 PM");
 
 
 });
